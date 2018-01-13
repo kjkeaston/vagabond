8aec22111bcfcd5c45596b9cacd94f3d6e0d51e4
AdamMenard <adammenard888@gmail.com>
2018-01-12 16:09:33 -0800

    added seed info, still incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+City.destroy_all
+Post.destroy_all
+
+User.create ([
+  {
+    first_name: "Bill",
+    last_name: "Smith",
+    current_city: "London",
+    email: "billsmith@email.com",
+    password: "bill"
+  },
+  {
+    first_name: "Bob",
+    last_name: "Brown",
+    current_city: "Houston",
+    email: "bobbrown@email.com",
+    password: "bob"
+  },
+  {
+    first_name: "Linda",
+    last_name: "Jones",
+    current_city: "San Francisco",
+    email: "lindajones@email.com",
+    password: "linda"
+  }
+  ])
+
+City.create ([
+  {
+  name: "London",
+  city_image: "http://atom.mu/wp-content/uploads/2017/01/London-Expat-Explore-Xmas-2017.jpg"}
+  },
+  {
+  name: "San Franciso",
+  city_image: "http://www.gertchristen.org/wordpress/wp-content/uploads/2016/03/san-francisco-san-francisco-bay-1112x630.jpg"
+  },
+  {
+  name: "Paris",
+  city_image: "http://www.telegraph.co.uk/content/dam/Travel/2016/August/Paris-travel-AP65117955-xlarge.jpg"})
+  ])
+
+Post.create ([
+  {
+
+  },
+
+  ])
