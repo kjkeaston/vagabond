cb0ff73d8276e245fd7b19b02765dd67257dec71
AdamMenard <adammenard888@gmail.com>
2018-01-17 09:32:52 -0800

    made the add post hidden if you are not logged in

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,6 +7,7 @@ class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
     @post = Post.find_by(id: post_url)
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   private
