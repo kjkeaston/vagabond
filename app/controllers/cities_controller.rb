aa0d7068573de6adf3e7b0a9e18c76301c8e2a60
AdamMenard <adammenard888@gmail.com>
2018-01-17 01:05:59 -0800

    definded current user instance variables in users and posts show views

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,7 +6,7 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.find(params[:id])
-    # @post = Post.find_by(id: post_url)
+    @post = Post.find_by(id: post_url)
   end
 
   private
